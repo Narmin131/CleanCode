f40e2dc1a8233789faa5ba23efe47076dd064aeb
Narmin <mnermin064@gmail.com>
2023-07-08 15:25:13 +0400

    Bugs fixed

--- Redux/lesson11/src/pages/Blogs.jsx
+++ Redux/lesson11/src/pages/Blogs.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { useSelector } from "react-redux";
 import BlogCard from "../components/BlogCard";
 
 const Blogs = () => {
   const myBLogs = useSelector((store) => store.AppReducer);
+
+
+  useEffect(() => {
+    localStorage.setItem("Blogs", JSON.stringify(myBLogs));
+  }, [myBLogs]);
 
   return (
     <div className="container">
